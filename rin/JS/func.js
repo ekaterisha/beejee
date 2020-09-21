32d47b6346724e9d272503b545adc3832888cd22
Rina <rina@gitlab.com>
2020-09-22 00:24:13 +0300

    css + some clean

--- rin/JS/func.js
+++ rin/JS/func.js
@@ -19,36 +19,5 @@ function taskStatus(id){
     } );
 }
 
-$('#testing_form').submit(function(){
-    
-    if($(this).attr('validated')=='false'){
-        var email = $('input[name="email"]').val();
-        var test_id = $('#testing_form').attr('test_id');
-        
-        $.ajax({ 
-            type: 'POST',
-            url: 'index.php?r=Testing&action=check',
-            data: 'email='+email+"&test_id="+test_id,
-            success: function(answer){  
-                if(answer == 'no'){
-                    $('#testing_form').attr('validated',"true");
-                    $('#testing_form').submit();
-                } 
-                if(answer == 'yes'){
-                    alert('Такой пользователь уже отправлял ответы!');
-                }
-            }
-     });
-     return false;  
-    }                            
-   return true;
-});
 
-
-$('[type="radio"]').change(function() {
-    name = $(this).attr('name');
-    question_id = name.substr(0, name.indexOf('['));
-
-    $('[type="radio"]').filter("[name*='"+question_id+"[']").not(this).prop('checked', false);
-});
 
--- rin/Views/MainView.php
+++ rin/Views/MainView.php
@@ -12,7 +12,7 @@ $page .= '<head><meta charset="utf-8">
   <h4>Задачник</h4>
   </span>
   <span class="right">
-    <button class="button btn-success"> <h4>Войти </h4></button>
+    <button class="button btn-login"> <h4>Войти </h4></button>
     <img src="rin/Media/user.png" ><br>
   </span>
 </div>
@@ -23,81 +23,104 @@ $page .= '<head><meta charset="utf-8">
   </div>
 </div>
 <div class="tasks">
-<h2>АКТУАЛЬНЫЕ ЗАДАЧИ</h2>
-<p>Сортировка </p>
+  <div>
+  <h2>АКТУАЛЬНЫЕ ЗАДАЧИ</h2>
+  <h4>Сортировка </h4>
+  <div/>
 ';
 foreach ($this->model->Data as $key=>$value) {
   $checked = $value['status'] == 'done' ? 'checked' : '';
-    $page .= '<section> <div>
-              <span class="span sectionText"> <p>Task</p></span> <span class="span sectionCkbx">Выполнена <input type="checkbox" name="status'.$value['id'].'" '.$checked.' onclick="taskStatus('.$value['id'].')"></span>
-                        </div>';
-    $page .= '<div><span class="container">'.$value['fio'].'</span><br>';
-    $page .= '<span class="container">'.$value['email'].'</span><br>';
-    $page .= '<span class="text">'.$value['task_text'].'</span><br>';
+    $page .= '<section> 
+              <div> 
+              <div>
+              <span class="span sectionText">'.$value['fio'].'</span> 
+              <span class="span sectionCkbx"> Status <input class="input-ckbx" type="checkbox" name="status'.$value['id'].'" '.$checked.' onclick="taskStatus('.$value['id'].')"></span>
+               </div>';
+    $page .= '<span class="container">'.$value['email'].'</span> </div>';
+    $page .= '<div class="text">'.$value['task_text'].'</div>';
     $page .= '
     <div>
-    <button class="open-button" onclick="openForm('.$value['id'].')">Редачить</button>
+    <button class="open-button" onclick="openForm('.$value['id'].')">Редактировать</button>
     <div class="form-popup" id="myForm'.$value['id'].'">
     <form action="index.php?action=update&id='.$value['id'].'&now_page='.$this->now_page.'" class="form-container" method="post">
-      <h1>Отредактировать задачу</h1>
-      <label for="fio"><b>Имя</b></label><br>
-      <input type="text" placeholder="Ваше имя" name="fio" value="'.$value['fio'].'" required><br>
-      <label for="email"><b>Е-мейл</b></label><br>
-      <input type="text" placeholder="Ваш е-мейл" name="email" value="'.$value['email'].'" required><br>
-      <label for="task_text"><b>Описание задачи</b></label><br>
-      <textarea placeholder="Описание задачи" rows="5" cols="42" name="task_text" required>'.$value['task_text'].'</textarea><br>
-      <button type="submit" class="btn">Принять</button>
-      <button type="button" class="btn cancel" onclick="closeForm('.$value['id'].')">Закрыть</button>
+      <div class="addtaskform">  
+        <h2>РЕДАКТИРОВАНИЕ ЗАДАЧИ</h2>
+      </div>
+      <div class="addtaskform">
+        <span class="left-add"> 
+          <label for="fio"><h4>Имя</h4></label><br>
+          <input class="input" type="text" placeholder="Ваше имя" name="fio" value="'.$value['fio'].'" required><br>
+        </span>
+        <span class="right-add">
+          <label for="email"><h4>Е-мейл</h4></label><br>
+          <input class="input" type="text" placeholder="Ваш е-мейл" name="email" value="'.$value['email'].'" required><br>
+        </span>
+      </div>
+      <div class="addtaskform">
+      <span class="left-add">
+        <label for="task_text"><h4>Описание задачи</h4></label><br>
+        <textarea class="input-desc" placeholder="Описание задачи" rows="5" cols="42" name="task_text" required>'.$value['task_text'].'</textarea><br>
+      </span>
+      </div>
+      <div class="addtaskform-btn">
+      <button type="submit" class="add-button">Принять</button>
+      <button type="button" class="reset-button" onclick="closeForm('.$value['id'].')">Закрыть</button>
+      </div>
     </form>
   </div>
     <form action="index.php?action=delete&id='.$value['id'].'&now_page='.$this->now_page.'" method="post">
-      <input type="submit" value="Удалить">
+      <input type="submit"  class="delete-button" value="Удалить">
     </form></div>
    </section>';
 }
 
 
-$page .= '<p>Навигация</p>
-<section class="pagination">';
+$page .= '
+<br>
+<div class="pagination">';
 if ($this->now_page>1){
-  $page .='<a class="pagination a" href="index.php?page='.($this->now_page-1).'">❮</a>';
+  $page .='<a class="pagination a" href="index.php?page='.($this->now_page-1).'">Назад</a>';
 }
 
 for ($i = 1; $i <= $this->last_page; $i++) {
   $page .= '<a class="pagination a" href="index.php?page='.$i.'" >'.$i.'</a>';
 }
 if ($this->now_page<$this->last_page){
-  $page .='<a class="pagination a" href="index.php?page='.($this->now_page+1).'">❯</a>';
+  $page .='<a class="pagination a" href="index.php?page='.($this->now_page+1).'">Дальше</a>';
 }
 
 $page .= '
-</section>
 </div>
-<div class="addtaskform">
-<form id="task_form" action="index.php?action=create" method="post">
-<p>Добавить задачу в список<Br>
-<p><b>Ваше имя:</b><br>
-<input type="text" name="fio" placeholder="Александров Александр Александрович" size="40" required></p>
-<p><b>Электронная почта:</b><Br>
-<input type="email" name="email" placeholder="exemple@gmail.com" size="40" required><Br>
-</p>
-<p><b>Описание задачи:</b><Br>
-<textarea placeholder="Опишите максимально детально задачу" rows="5" cols="42" name="task_text" required></textarea><Br>
-</p>
-<p><b>Статус</b><br>
-<p class="string"><b>Выполнена</b>
-<input type="checkbox" name="status" value="done" >
-</p>
-
-<p><input type="submit" value="Добавить">
-<input type="reset" value="Очистить"></p>
-
-
-</form></div>';
+</div>
+<div>
+  <form id="task_form" action="index.php?action=create" method="post">
+    <div class="addtaskform">
+      <h2>ДОБАВИТЬ НОВУЮ ЗАДАЧУ</h2>
+    </div>
+    <div class="addtaskform">
+      <span class="left-add"><h4>Имя</h4><br>
+      <input type="text" class="input" name="fio" placeholder="Александров Александр Александрович" size="40" required></span>
+      <span class="right-add"><h4>E-mail</h4><Br>
+      <input type="email" class="input" name="email" placeholder="exemple@gmail.com" required>
+      </span>
+    </div>
+    <div class="addtaskform">
+      <span class="left-add"><h4>Описание задачи<h4><Br>
+      <textarea class="input-desc" placeholder="Опишите максимально детально задачу"  rows="5" cols="42" name="task_text" required></textarea><Br>
+      </span>
+    </div>
+    <div class="addtaskform" style="height: 65px;">
+      <div class="addtaskform-btn">
+        <input class="reset-button" type="reset" value="Очистить">
+        <input class="add-button" type="submit" value="Добавить">
+      </div>
+    </div>
+  </form>
+</div>';
 $page .= '
-<div class="contacts"> 
-  <p> OОО Рина </p>
-  <p> E-mail: ekaterisha48@gmail.com </p>
+<div> 
+  <p class="contacts"> Градковская Рина </p>
+  <p class="contacts"> E-mail: <a href="mailto:ekaterisha48@gmail.com?subject=Вопрос по Задачнику"> ekaterisha48@gmail.com </a> </p>
 </div> 
 </body>
 
